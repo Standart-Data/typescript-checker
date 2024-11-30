8ce3ac9af45359665d024f72601c8b0ccffe8a74
Pavel Rudenko <rudenkopavel21@gmail.com>
2024-11-30 12:30:43 +0300

    TS-Analizer вывод каждой сущности

--- exercise_1/test_ast.js
+++ exercise_1/test_ast.js
@@ -1,46 +1,164 @@
-const assert = require('assert');
-const ts = require('typescript');
+const fs = require("fs");
+const ts = require("typescript");
 
+async function readTsFiles(filePaths) {
+  try {
+    const allVariables = {
+      variables: {},
+      interfaces: {},
+      functions: {},
+      types: {},
+      enums: {},
+      classes: {},
+    };
 
-const filename = "main.ts";
-const program = ts.createProgram([filename], {});
-const sourceFile = program.getSourceFile(filename);
-const typeChecker = program.getTypeChecker();
-
-const allVariables = {}
-
-// Здесь составляется дерево
-function recursivelyGetVariableDeclarations(node, sourceFile) {
-    if (node.kind === ts.SyntaxKind.VariableDeclaration)  {
-
-        const nodeText = node.getText(sourceFile);
-        const type = typeChecker.getTypeAtLocation(node);
-        const typeName = typeChecker.typeToString(type, node).split("|");
-        const name = node.name.getText(sourceFile);
-
-        allVariables[name] = ({"name": name, "type": typeName})
-
+    function parseObject(node) {
+      const obj = {};
+      ts.forEachChild(node, (property) => {
+        if (ts.isPropertyAssignment(property)) {
+          const name = property.name.getText();
+          if (ts.isObjectLiteralExpression(property.initializer)) {
+            obj[name] = {
+              types: ["object"],
+              value: parseObject(property.initializer),
+            };
+          } else if (ts.isArrayLiteralExpression(property.initializer)) {
+            obj[name] = {
+              types: ["array"],
+              value: property.initializer.elements.map((el) =>
+                el.getText().replace(/['"]+/g, "")
+              ),
+            };
+          } else {
+            obj[name] = {
+              types: ["string"],
+              value: property.initializer.getText().replace(/['"]+/g, ""),
+            };
+          }
+        }
+      });
+      return obj;
     }
 
-    node.forEachChild(child =>
-        recursivelyGetVariableDeclarations(child, sourceFile)
-    );
+    function parseNode(node) {
+      switch (node.kind) {
+        case ts.SyntaxKind.VariableStatement:
+          node.declarationList.declarations.forEach((declaration) => {
+            const name = declaration.name.getText();
+            const type = declaration.type
+              ? declaration.type.getText().trim()
+              : "unknown";
+            const initializer = declaration.initializer
+              ? declaration.initializer
+              : null;
+
+            if (initializer && ts.isObjectLiteralExpression(initializer)) {
+              allVariables.variables[name] = {
+                types: [type],
+                value: parseObject(initializer),
+              };
+            } else {
+              allVariables.variables[name] = {
+                types: [type],
+                value: initializer
+                  ? initializer.getText().trim().replace(/['"]+/g, "")
+                  : null,
+              };
+            }
+          });
+          break;
+        case ts.SyntaxKind.InterfaceDeclaration:
+          const interfaceName = node.name.getText();
+          const properties = node.members.map((member) => ({
+            name: member.name.getText(),
+            type: member.type.getText().trim(),
+          }));
+          allVariables.interfaces[interfaceName] = { properties };
+          break;
+        case ts.SyntaxKind.FunctionDeclaration:
+          const functionName = node.name.getText();
+          const params = node.parameters.map((param) => param.getText().trim());
+          const returnType = node.type ? node.type.getText().trim() : "void";
+          allVariables.functions[functionName] = {
+            types: ["function"],
+            params,
+            returnResult: [returnType],
+          };
+          break;
+        case ts.SyntaxKind.TypeAliasDeclaration:
+          const typeName = node.name.getText();
+          const type = node.type.getText().trim();
+          allVariables.types[typeName] = type;
+          break;
+        case ts.SyntaxKind.EnumDeclaration:
+          const enumName = node.name.getText();
+          const members = node.members.map((member) => member.name.getText());
+          allVariables.enums[enumName] = members;
+          break;
+        case ts.SyntaxKind.ClassDeclaration:
+          const className = node.name.getText();
+          const classMembers = {};
+          node.members.forEach((member) => {
+            if (ts.isMethodDeclaration(member)) {
+              const methodName = member.name.getText();
+              const methodParams = member.parameters.map((param) =>
+                param.getText().trim()
+              );
+              const methodReturnType = member.type
+                ? member.type.getText().trim()
+                : "void";
+              classMembers[methodName] = {
+                types: ["function"],
+                params: methodParams,
+                returnResult: [methodReturnType],
+              };
+            } else if (ts.isPropertyDeclaration(member)) {
+              const propertyName = member.name.getText();
+              const propertyType = member.type
+                ? member.type.getText().trim()
+                : "unknown";
+              classMembers[propertyName] = {
+                types: [propertyType],
+                value: null,
+              };
+            }
+          });
+          allVariables.classes[className] = classMembers;
+          break;
+        default:
+          break;
+      }
+    }
+
+    for (const filePath of filePaths) {
+      const data = await fs.promises.readFile(filePath, "utf-8");
+      const sourceFile = ts.createSourceFile(
+        filePath,
+        data,
+        ts.ScriptTarget.Latest,
+        true
+      );
+
+      ts.forEachChild(sourceFile, parseNode);
+    }
+
+    console.log("Все найденные элементы:", allVariables);
+    return allVariables;
+  } catch (err) {
+    console.error("Ошибка при чтении файлов:", err);
+  }
 }
 
-recursivelyGetVariableDeclarations(sourceFile, sourceFile);
+// readTsFiles(["main.ts", "unique_typescript.ts"]);
 
-// Тут показываем какие у нас объекты объявленя
+readTsFiles(["main.ts", "unique_typescript.ts"]);
 
+// describe("First test", function () {
+//   it("В коде объявлена переменная a c типом number", function () {
+//     assert.ok(allVariables["a"]["type"].includes("number"));
+//   });
 
-
-describe('First test', function () {
-
-    it('В коде объявлена переменная a c типом number', function () {
-        assert.ok(allVariables["a"]["type"].includes("number"))
-    });
-
-    it('В коде объявлена переменная b c типом string', function () {
-        assert.ok(allVariables["b"]["type"].includes("string"))
-    });
-
-});
+//   it("В коде объявлена переменная b c типом string", function () {
+//     assert.ok(allVariables["b"]["type"].includes("string"));
+//   });
+// });
